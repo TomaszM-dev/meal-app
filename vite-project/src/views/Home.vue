fcac8a17f5da013c7ecf77a9d7fce5d757382b7b
TomaszM-dev <tomekmalocha@gmail.com>
2024-03-08 15:48:02 +0100

    first commit

---
+++ vite-project/src/views/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { computed } from 'vue'
+import store from '../store'
+
+const meals = computed(() => store.state.meals)
+
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+</script>
+
+<template>
+  <div class="flex flex-col p-8 justify-center gap-10  ">
+
+    <input placeholder="search for meals" type="text" class="rounded border-2 border-gray-200 w-fit " />
+
+    <div class="flex justify-center gap-2">
+      <router-link :to="{ name: 'byLetter', params: { letter } }" v-for="(letter, index) of letters" :key="index">{{
+        letter
+      }}</router-link>
+    </div>
+  </div>
+</template>
---
+++ vite-project/src/views/MealList.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div>
+    meal list
+  </div>
+</template>
+<style scope></style>
